ce44f563f447a4ecb72d3b6ff10087080bc36d0e
borjaocerin <borja.ocerinjauregui@opendeusto.es>
2023-04-03 00:11:40 +0200

    funciona insertar

--- BD/funcionesBD.c
+++ BD/funcionesBD.c
@@ -4,6 +4,8 @@
 #include "../Empleado/empleado.h"
 #include "../Actividad/actividad.h"
 #include "../Cliente/cliente.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 sqlite3 *db;
 sqlite3_stmt *stmt;
@@ -63,7 +65,7 @@ void ShowActivitiesInCommunity(char comunidad[])
 
 void InsertActivity(char nombre[], char dificultad[], int per_min, int per_max, int edad_min)
 {
-	sqlite3_open("DeustoAventura.db", &db);
+	sqlite3_open("DB/DeustoAventura.db", &db);
 
 	char sql[] = "insert into ACTIVIDAD (NOMBRE_ACT, DIFICULTAD, LIMITE_PER_MIN, LIMITE_PER_MAX, EDAD_MIN) values (?, ?, ?, ?, ?)";
 
@@ -144,33 +146,36 @@ void ShowWorkers()
 	sqlite3_close(db);
 }
 
-void InsertWorker(char* dni, char *nombre, char *apellido, int telefono, char* correo, char *contrasena,char* estatus, int cod_park)
-{
-	sqlite3_open("../DeustoAventura.db", &db);
 
-	char sql[] = "insert into EMPLEADO (COD_EMP, DNI, NOMBRE_EMP, APELLIDO_EMP, TFNO, CORREO, CONTRASENA, ESTATUS, COD_PARK) values (NULL, ?, ?, ?, ?, ?, ?, ?, ?)";
 
-	sqlite3_prepare_v2(db, sql, strlen(sql) + 1, &stmt, NULL) ;
 
-	sqlite3_bind_text(stmt, 1, dni, strlen(dni), SQLITE_STATIC);
-	sqlite3_bind_text(stmt, 2, nombre, strlen(nombre), SQLITE_STATIC);
-	sqlite3_bind_text(stmt, 3, apellido, strlen(apellido), SQLITE_STATIC);
-	sqlite3_bind_int(stmt, 4, telefono);
-	sqlite3_bind_text(stmt, 5, correo, strlen(correo), SQLITE_STATIC);
-	sqlite3_bind_text(stmt, 6, contrasena, strlen(contrasena), SQLITE_STATIC);
-	sqlite3_bind_text(stmt, 7, estatus, strlen(estatus), SQLITE_STATIC);
-	sqlite3_bind_int(stmt, 8, cod_park);
+void InsertWorker(char* dni, char *nombre, char *apellido, int telefono, char* correo, char *contrasena,char* estatus, int cod_park) {
+	   sqlite3 *db;
+	    char *error = 0;
+	    int rc;
 
-	result = sqlite3_step(stmt);
+	    rc = sqlite3_open("DeustoAventura.db", &db);
 
-	if (result != SQLITE_DONE) {
-		printf("Error insertando el empleado\n");
-	}else{
-		printf("Empleado insertado - >  DNI: %s - Nombre Y Apellido: %s %s - TFNO: %i - Correo: %s - Estatus: %s - Codigo Parque: %i\n", dni, nombre, apellido, telefono, correo, estatus, cod_park);
-	}
+	    if (rc == SQLITE_OK) {
+	        printf("Conexión establecida\n");
 
-	sqlite3_finalize(stmt);
-	sqlite3_close(db);
+	        char query[400];
+	        sprintf(query, "INSERT INTO EMPLEADO ( DNI, NOMBRE_EMP, APELLIDO_EMP, TFNO, CORREO,  CONTRASENA, ESTATUS, COD_PARK) VALUES ('%s', '%s', '%s', %d, '%s', '%s', '%s', %d)", dni, nombre, apellido, telefono, correo, contrasena, estatus, cod_park);
+
+	        rc = sqlite3_exec(db, query, 0, 0, &error);
+
+	        if (rc == SQLITE_OK) {
+	            printf("Trabajador insertado correctamente\n");
+	        } else {
+	            printf("Error al insertar trabajador: %s\n", error);
+	        }
+	    } else {
+	        printf("Error al conectar a la base de datos: %s\n", sqlite3_errmsg(db));
+	    }
+
+		sqlite3_finalize(stmt);
+	    sqlite3_close(db);
+
 }
 
 void isWorker(char nombre[], char contrasena[]){
@@ -205,22 +210,28 @@ void isWorker(char nombre[], char contrasena[]){
 
     		printf("\n");
 
+    		sqlite3_finalize(stmt);
+    	    sqlite3_close(db);
     		menuJefe();
     	} else if(emp.estatus[0] == 'E'){
     		printf("Se ha iniciado sesion como EMPLEADO con el alias ");
     		puts(nombre);
 
     		printf("\n");
+    		sqlite3_finalize(stmt);
+    			    sqlite3_close(db);
+
 
     		menuEmpleado();
     	} else {
+    		sqlite3_finalize(stmt);
+
+    		    sqlite3_close(db);
     		printf("Empleado no encontrado");
     		main();
     	}
 
-    sqlite3_finalize(stmt);
 
-    sqlite3_close(db);
 }
 
 
